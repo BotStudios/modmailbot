c5fbb676642dd87dfae49a1fcec4d27fcedacfef
CY L <91641514+w3cy@users.noreply.github.com>
2021-12-16 00:59:00 +0800

    Create errors.js

---
+++ src/errors.js
@@ -0,0 +1,27 @@
+const config = require('./config');
+const { Util } = require('discord.js');
+const errors = {
+    throwError: (error) => { if(!error)return; throw new Error(`${error}`); },
+    provide: (value) => errors.throwError(`Please provide a ${value}`),
+    configCheck: () => {
+      if(typeof config?.token != 'string') return errors.provide('valid token');
+      if(typeof config?.databaseURI != 'string') return errors.provide('valid database URI');
+      if(typeof config?.guildID != 'string')return errors.provide('valid guildID');
+      if(typeof config?.roleID != 'string')return errors.provide('valid role ID');
+      if(typeof config?.category != 'string')return errors.provide('valid category ID');
+      if(typeof config?.prefix != 'string')return errors.provide('valid prefix');
+      if(config?.logThreads == true && !config?.logsURI)return errors.provide('valid modmail logs URI');
+      if(config?.port && typeof config?.port != 'number')return errors.provide('valid port');
+      if(!config?.colors?.primary)return errors.provide('primary color');
+      if(!config?.colors?.success)return errors.provide('success color');
+      if(!config?.colors?.error)return errors.provide('error color');
+      if(!config?.colors?.custom)return errors.provide('custom color');
+      for(i in config.colors) { Util.resolveColor(config?.colors[i]); }
+    },
+    reply: (message, value) => message.reply({ embeds: [{ description: `${value}`, color: `${config?.colors?.error}` }] }),
+    exceedLimit: (message, value, max) => errors.reply(message, `${value} cannot be longer than ${max} characters.`),
+    provideAValue: (message) => errors.reply(message, 'Please provide a value.'),
+    provideATag: (message) => errors.reply(message, 'Please provide a tag name.'),
+    tagDoesNotExist: (message) => errors.reply(message, 'This tag does not exist.')
+};
+module.exports = errors;
