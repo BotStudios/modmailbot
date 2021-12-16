fe23704e40893bbc007de5e3e632a75128973b65
CY L <91641514+w3cy@users.noreply.github.com>
2021-12-16 22:56:25 +0800

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    databaseURI: '',
+    port: 3000,
+    token: '',
+    guildID: '',
+    category: '',
+    roleID: '',
+    prefix: '',  
+    logThreads: false,
+    notifyMsg: '',
+    webhookURI: '',
+    logsURI: '',
+    colors: { 
+        success: '',
+        error: '',
+        primary: '',
+        custom: ''
+    }
+}
