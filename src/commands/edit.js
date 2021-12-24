82e51c267060f3df7c53c9824dd8c589db75f0d4
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-24 13:51:03 +0800

    Create edit.js

---
+++ src/commands/edit.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'edit',
+    description: '',
+    run: async ({ bot, data, config, message, args }) => {
+        if(!data)return; 
+        if(!message?.reference?.messageId) return bot.shortMessage(message, 'Please refer to a message.', 'error');
+        if(!args.join(" "))return bot.shortMessage(message, 'Please provide a message to be edited.', 'error');
+        var msg = bot.editMsg.get(message?.reference?.messageId);
+        if(!msg)return bot.shortMessage(message, 'This message cannot be edited.', 'error');
+        return msg.edit(bot.getReplyContent(message, args.join(" "), true)).then(bot.shortMessage(message, `\`\`\`fix\n${args.join(" ")}\`\`\``, 'success', { name: `Message Edited By ${message?.author?.tag}`, icon_url: `${message?.author?.avatarURL()}`}, { text: `ID : ${msg?.id}` }));
+    }
+}
