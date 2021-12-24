2c11b2d6bfd849a27f89e4c2a10168ec70de772e
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-24 13:47:59 +0800

    Update manager.js

--- src/manager.js
+++ src/manager.js
@@ -1,14 +1,13 @@
-const Discord = require('discord.js');
 const Utils = require('./utils');
 const config = require('./config.js');
 
 class Manager extends Utils {
   constructor(client, collection) {
-    super(Discord, client, collection);
+    super(client, collection);
     this.collection = collection;
     this.client = client;
     this.cooldown = new Set();
-    this.editMsg = new Discord.Collection();
+    this.editMsg = new this.Discord.Collection();
     this.reservedCommand = ['delete', 'close', 'tags', 'tag', 'log', 'block', 'unblock', 'blocked', 'edit'];
 }
 
@@ -17,127 +16,9 @@ async command(message) {
    if(!message?.member?.roles?.cache.some(role => role.id === `${config?.roleID}`))return;
    const data = await this.model.findOne({ Channel: message.channel.id });
    const args = message.content.slice(config?.prefix?.length).split(/ +/);
-   const command = args.shift().toLowerCase();
-
-   if(command == "close") {
-    if(!data)return; 
-    await this.deleteThread({ Channel: message.channel.id }, message.author);
-   }else if(command == "delete"){
-    if(!args[0])return this.shortMessage(message, 'Please provide a user ID.', 'error');
-    await this.deleteThread({ User: args[0] }, message.author).then(() => message.reply({ embeds: [{ description: 'This thread will be deleted...'}] })).catch(err=> {});
-   }else if(command == "tags") { 
-     var embed = new Discord.MessageEmbed().setAuthor('Snippets'); 
-     var tags = ""; var tagCount = 0;
-     const data = await this.getTags(); 
-     for(var i in data) { tagCount++; tags += `${tagCount}. ${i}\n`; }
-     if(tagCount == 0) tags += `The tag list was empty at the moment`;
-     embed.setDescription(tags).setColor('RANDOM'); message.reply({ embeds: [embed] });
-   }else if(command == "tag"){
-       if(!args[0])return this.error.provideATag(message);
-       switch(args[0]) {
-           case "create":
-            if(!args[1])return this.error.provideATag(message);
-            if(!args[2])return this.error.provideAValue(message);
-            if(args[1].length > 20)return this.error.exceedLimit(message, 'Tag name', 20);
-            var tagValue = args?.splice(2)?.join(" ")
-            if(tagValue > 500 || tagValue < 1)return this.error.exceedLimit(message, 'Tag value', 500);
-            await this.createTag(args[1], tagValue);
-            return this.shortMessage(message, await this.getTag(args[1]), 'custom', { name: `Snippet for ${args[1]}` }).catch(console.log)
-            break;
-            case "delete":
-            if(!args[1])return this.error.provideATag(message);
-            const wasDeleted = await this.deleteTag(args[1]);    
-            if(!wasDeleted)return this.error.tagDoesNotExist(message);
-            return this.shortMessage(message, `**${args[1]}** was removed from the tag list.`, 'error');  
-            break;       
-            default: 
-            var info = await this.getTag(args[0]);
-            if(!info)return this.shortMessage(message, `This log does not exist.`, 'error');
-            return this.shortMessage(message, info, 'custom', { name: `Snippet for ${args[0]}` });
-            
-
-        }
-   }else if(command == "log") {
-     if(!config?.logThreads)return this.shortMessage(message, 'Modmail logs are disabled.', 'error');
-      switch(args[0]) { 
-          case "delete":
-          if(config?.secureLogs == true)return this.shortMessage(message, "Modmail logs are protected.", 'error', { name: 'Secure Logs'})
-          if(!args[1])return this.shortMessage(message, 'Please provide a log ID.', 'custom');
-          const wasDeleted = await this.deleteLog(args[1], message?.author);   
-          if(!wasDeleted)return this.shortMessage(message, 'This log does not exist.', 'error', { name: 'Error' });
-          return this.shortMessage(message, `**${args[1]}** was removed from the modmail log.`, 'error');  
-          break;
-          case "view":
-          if(config?.secureLogs == true)return this.shortMessage(message, "Modmail logs are protected.", 'error', { name: 'Secure Logs'});
-          if(!args[1])return this.shortMessage(message, 'Please provide a log ID.', 'custom');
-          const data = await this.getLog(args[1]);
-          if(!data)return this.shortMessage(message, 'This log does not exist.', 'error', { name: 'Error' });
-          const content = JSON.stringify({
-            User: data?.User,
-            Channel: data?.Channel,
-            Messages: data?.Messages,
-            Id: data?.Id
-          });
-          return message.reply({ files: [ new this.Discord.MessageAttachment(Buffer.from(content, 'utf-8'), 'message.txt') ] })
-          break;
-          case "this":
-          const id = message?.channel?.topic?.slice(3);
-          if(!id)return this.shortMessage(message, 'This is not a modmail thread channel.', 'error', { name: 'Invalid Channel' });
-          const user = await this.client.users.cache.get(id);
-          const dt = await this.getLogsByUserId(user.id);
-          var ct = ''; var dataCount = 0;
-          dt.forEach((e) => {
-          dataCount++; ct += `${dataCount}. [\`${e?.Id}\`](${config?.logsURI}/${e?.Id}) - <t:${e?.Timestamp}>\n`;
-          });
-          if(dataCount < 1)ct += `Couldn't find any records for ${user.tag}`;
-          return this.shortMessage(message, ct, 'custom', { name: `Thread logs for ${user.username}`, icon_url: user.avatarURL() });
-          break;
-          case "adduser": 
-          const usr = await this.client.users.cache.get(args[1]);
-          if(!usr) return this.shortMessage(message, 'Please provide a valid user ID', 'error', { name: 'Error' });
-          if((await this.getlogViewers())?.includes(usr.id)) return this.shortMessage(message, 'This user can already view the logs', 'error');
-          await this.addLogViewer(usr.id);
-          this.shortMessage(message, `Successfully added <@!${usr.id}>(${usr.id}) to the logs viewer list.`, 'success', { name: 'Log Viewer Added' });
-          break;
-          case "removeuser":
-          const remUsr = await this.removelogViewer(args[1]);
-          if(!remUsr)return this.shortMessage(message, 'This user cannot view the logs.', 'error');
-          return this.shortMessage(message, `Successfully removed <@!${args[1]}>(${args[1]}) from the log viewer list.`, 'success', { name: 'Log Viewer Remove' });
-          break;
-          case "viewers":
-         const viewers = await this.getlogViewers();
-         return this.listingEmbed(message, viewers, "No one can view the logs.", "Logs Viewer");
-          break;          
-          default:
-            if(!args[0])return this.shortMessage(message, 'Please provide a log ID.', 'error');
-            var info = await this.getLog(args[0]);
-            if(!info)return this.shortMessage(message, 'This log doesn not exist.', 'error', { name: 'Error' });
-            return this.shortMessage(message, `**User ID:** \`${info.User}\`\n**Channel ID:** \`${info.Channel}\``, 'custom', { name: 'Modmail Log', url: `${config?.logsURI}/${info.Id}` })
-      }
-   }else if(command == "block") {
-      var id = message?.channel?.topic?.slice(3);
-      if(args[0]) id = args[0];
-      const userID = (await this.client.users.cache.get(id))?.id;
-      if(!userID)return this.shortMessage(message, 'Couldn\'t find this user.', 'error', { name: 'Error' });
-      const blocked = await this.blockUser(userID, message?.author);
-      if(!blocked)return this.shortMessage(message, 'This user was blocked.', 'error', { name: 'Error' });
-      return this.shortMessage(message, `Successfully blocked <@!${userID}>(${userID})`, 'success', { name: 'User Blocked' });
-   }else if(command == "unblock") {
-     if(!args[0])return this.shortMessage(message, 'Please provide a user ID.', 'error');
-     const unblocked = await this.unblockUser(args[0], message?.author);
-     if(!unblocked)return this.shortMessage(message, 'This is not a blocked user.', 'error');
-     return this.shortMessage(message, `Successfully unblocked <@!${args[0]}>(${args[0]})`, 'success', { name: 'User Unblocked' });
-   }else if(command == "blocked") {
-     const blocked = await this.getBlockedUsers();     
-     return this.listingEmbed(message, blocked, "No one was blocked.", "Blocked Users");
-   }else if(command == "edit"){
-    if(!data)return; 
-    if(!message?.reference?.messageId) return this.shortMessage(message, 'Please refer to a message.', 'error');
-    if(!args.join(" "))return this.shortMessage(message, 'Please provide a message to be edited.', 'error');
-    var msg = this.editMsg.get(message?.reference?.messageId);
-    if(!msg)return this.shortMessage(message, 'This message cannot be edited.', 'error');
-    return msg.edit(this.getReplyContent(message, args.join(" "), true)).then(this.shortMessage(message, `\`\`\`fix\n${args.join(" ")}\`\`\``, 'success', { name: `Message Edited By ${message?.author?.tag}`, icon_url: `${message?.author?.avatarURL()}`}, { text: `ID : ${msg?.id}` }));
-   }else { await this.replyThread(message); }   
+   const command = this.commands.get(args.shift().toLowerCase());
+   if(!command) return await this.replyThread(message);  
+   try { await command.run({ bot: this, data, config, message, args }) } catch (error) { await message.reply({ embeds: [{ description: 'Something Went Wrong', color: '#e83838' }], ephemeral: true }) }
 }
 
 
@@ -194,7 +75,7 @@ try{  await message.react('✅') }catch(e) {}
 }
 
   inboxEmbed(message, firstMsg) {
-  var embed = new Discord.MessageEmbed()
+  var embed = new this.Discord.MessageEmbed()
    .setTimestamp()
    .setFooter(`ID : ${message.author.id}`)
    .setColor(`${config?.colors?.custom}`)
@@ -218,7 +99,7 @@ async sendInbox(message, firstMsg = false) {
     if(!data)return await this.createChannel(message);
     if (message.attachments.size > 0) {
     message.attachments.forEach((e) => {
-     embeds.push(new Discord.MessageEmbed().setDescription(`${e.proxyURL || ""}`).setImage(`${e.proxyURL || ""}`).setColor(`${config?.colors?.custom}`));
+     embeds.push(new this.Discord.MessageEmbed().setDescription(`${e.proxyURL || ""}`).setImage(`${e.proxyURL || ""}`).setColor(`${config?.colors?.custom}`));
     });
    }
    if (data) {
@@ -270,9 +151,6 @@ setTimeout(async () => {
    await this.sendInbox(message, true).then((m) => m.pin()).catch(err => {});
 }, 1000)
   }
-
-
-  
 }
 
 module.exports = Manager;
