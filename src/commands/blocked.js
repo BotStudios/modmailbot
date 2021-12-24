c2552ce6c157f13a3d132edcfc353f08f1eaab5c
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-24 15:26:32 +0800

    Update blocked.js

--- src/commands/blocked.js
+++ src/commands/blocked.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'blocked',
-    description: '',
+    description: 'A list of blocked users',
     run: async ({ bot, data, config, message, args }) => {
         const blocked = await bot.getBlockedUsers();     
         return bot.listingEmbed(message, blocked, "No one was blocked.", "Blocked Users");
