26b696b66c5727f5be49720c1397457836f0ca04
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-24 15:26:06 +0800

    Create help.js

---
+++ src/commands/help.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'help',
+    description: 'Learn more about a specific command',
+    run: async ({ bot, message, config, args }) => {
+      if(args[0]) {
+       const command = bot.commands.get(args[0]); var fields = null; var fieldContents = '';
+       if(!command) return bot.shortMessage(message, `Couldn't Find This Command **(${args[0]})**`, 'error', { name: 'Command Not Found' });
+       content = command?.description;
+       if(Array.isArray(command?.options)) command?.options?.forEach((e) => {
+          fieldContents += `\`${e?.name}\` - ${e?.description}\n`
+       }); fields = [{ name: 'Options', value: fieldContents }];
+       bot.shortMessage(message, content, 'custom',  null, { text: `BotStudios/ModmailBot - ${config?.version}` }, `${config?.prefix}${command.name}`, fields)
+      }else {
+      var content = ``;
+      bot.commands.each((e) => {
+        content += `**\`${e?.name}\`** - ${e?.description}\n`;
+      })
+      bot.shortMessage(message, content, 'custom',  null, { text: `BotStudios/ModmailBot - ${config?.version}` }, `Commands`)
+      }
+    }
+}
