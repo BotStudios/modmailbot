588c6d04c49f9e94756768b903021dca98b5436f
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-24 13:52:28 +0800

    Create tags.js

---
+++ src/commands/tags.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'tags',
+    description: '',
+    run: async ({ bot, config, message, args }) => {
+        var embed = new bot.Discord.MessageEmbed().setAuthor('Snippets'); 
+        var tags = ""; var tagCount = 0;
+        const data = await bot.getTags(); 
+        for(var i in data) { tagCount++; tags += `${tagCount}. ${i}\n`; }
+        if(tagCount == 0) tags += `The tag list was empty at the moment`;
+        embed.setDescription(tags).setColor('RANDOM'); message.reply({ embeds: [embed] });
+    }
+}
