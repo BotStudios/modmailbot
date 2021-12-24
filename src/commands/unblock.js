7213c21cdf2e3724606129e9082a7715c534459e
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-24 13:52:56 +0800

    Create unblock.js

---
+++ src/commands/unblock.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'unblock',
+    description: '',
+    run: async ({ bot, data, config, message, args }) => {
+        if(!args[0])return bot.shortMessage(message, 'Please provide a user ID.', 'error');
+        const unblocked = await bot.unblockUser(args[0], message?.author);
+        if(!unblocked)return bot.shortMessage(message, 'This is not a blocked user.', 'error');
+        return bot.shortMessage(message, `Successfully unblocked <@!${args[0]}>(${args[0]})`, 'success', { name: 'User Unblocked' });
+    }
+}
