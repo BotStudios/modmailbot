a96d71e36780419363cfa04fb28ab3342438987a
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-24 15:26:44 +0800

    Update close.js

--- src/commands/close.js
+++ src/commands/close.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'close',
-    description: '',
+    description: 'Close a thread',
     run: async ({ bot, data, config, message, args }) => {
         if(!data)return; 
         await bot.deleteThread({ Channel: message.channel.id }, message.author);
