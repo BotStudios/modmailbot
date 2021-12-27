41066d67183ecc6d3f44ec3f5e72aefc3be1ef7d
CY L <91641514+w3cy@users.noreply.github.com>
2021-12-28 00:37:55 +0800

    Update edit.js

--- src/commands/edit.js
+++ src/commands/edit.js
@@ -1,9 +1,12 @@
 module.exports = {
     name: 'edit',
     description: 'Edit a replied message',
+    options: [
+        { name: '[message]', description: 'Reply to a message to be edited' }
+    ],
     run: async ({ bot, data, config, message, args }) => {
         if(!data)return; 
-        if(!message?.reference?.messageId) return bot.shortMessage(message, 'Please refer to a message.', 'error');
+        if(!message?.reference?.messageId) return bot.shortMessage(message, 'Please refer/reply to a message.', 'error');
         if(!args.join(" "))return bot.shortMessage(message, 'Please provide a message to be edited.', 'error');
         var msg = bot.editMsg.get(message?.reference?.messageId);
         if(!msg)return bot.shortMessage(message, 'This message cannot be edited.', 'error');
