c129146daf7c5f333d6f12aa70c5ca85036e2177
CY L <91641514+w3cy@users.noreply.github.com>
2021-12-28 00:39:08 +0800

    Update unblock.js

--- src/commands/unblock.js
+++ src/commands/unblock.js
@@ -1,6 +1,9 @@
 module.exports = {
     name: 'unblock',
     description: 'Unblock a user',
+    options: [
+        { name: 'UserID', description: 'The ID of the user.' }
+    ],
     run: async ({ bot, data, config, message, args }) => {
         if(!args[0])return bot.shortMessage(message, 'Please provide a user ID.', 'error');
         const unblocked = await bot.unblockUser(args[0], message?.author);
