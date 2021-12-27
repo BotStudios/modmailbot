92a44b5bedb2fef72672dbeb3bd2c1f678ea358b
CY L <91641514+w3cy@users.noreply.github.com>
2021-12-28 00:37:41 +0800

    Update delete.js

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -1,6 +1,9 @@
 module.exports = {
-    name: 'delete',
+   name: 'delete',
    description: 'Delete (close) a specific thread',
+   options: [
+     { name: 'UserID', description: 'The ID of the user.' }
+   ],
    run: async ({ bot, data, config, message, args }) => {
     if(!args[0])return bot.shortMessage(message, 'Please provide a user ID.', 'error');
     await bot.deleteThread({ User: args[0] }, message.author).then(() => message.reply({ embeds: [{ description: 'This thread will be deleted...'}] })).catch(err=> {});
