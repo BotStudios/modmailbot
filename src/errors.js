50626cc9d033a01e4ce1faa05dc3dff9f25a3242
CY L <91641514+w3cy@users.noreply.github.com>
2021-12-31 18:10:33 +0800

    Update errors.js

--- src/errors.js
+++ src/errors.js
@@ -1,15 +1,18 @@
 const config = require('./config');
 const { Util } = require('discord.js');
+
 const errors = {
     throwError: (error) => { if(!error)return; throw new Error(`${error}`); },
     provide: (value) => errors.throwError(`Please provide a ${value}`),
-    configCheck: () => {
+    check: () => {
+      if (process.versions.node < "16.2.0")return errors.throwError("Please install node.js version 16.2.0 and above.");
       if(typeof config?.token != 'string') return errors.provide('valid token');
       if(typeof config?.databaseURI != 'string') return errors.provide('valid database URI');
       if(typeof config?.guildID != 'string')return errors.provide('valid guildID');
       if(typeof config?.roleID != 'string')return errors.provide('valid role ID');
       if(typeof config?.category != 'string')return errors.provide('valid category ID');
       if(typeof config?.prefix != 'string')return errors.provide('valid prefix');
+      if(config?.activity && typeof config?.activity != 'string')return errors.provide('valid bot status (activity)');
       if(config?.logThreads == true && !config?.logsURI)return errors.provide('valid modmail logs URI');
       if(config?.port && typeof config?.port != 'number')return errors.provide('valid port');
       if(!config?.colors?.primary)return errors.provide('primary color');
