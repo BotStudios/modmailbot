29fe379fd415317e2a19d28187f3a49277057c60
CY L <91641514+w3cy@users.noreply.github.com>
2022-01-01 15:45:19 +0800

    Create aEdit.js

---
+++ src/commands/aEdit.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'ae',
+    description: 'Edit a replied message anonymously',
+    options: [
+        { name: '[message]', description: 'Reply to a message to be edited' }
+    ],
+    run: async ({ bot, data, config, message, args }) => {
+        if(!data)return; 
+        if(!message?.reference?.messageId) return bot.shortMessage(message, 'Please refer/reply to a message.', 'error');
+        if(!args.join(" "))return bot.shortMessage(message, 'Please provide a message to be edited.', 'error');
+        var msg = bot.editMsg.get(message?.reference?.messageId);
+        if(!msg)return bot.shortMessage(message, 'This message cannot be edited.', 'error');
+        return await msg.edit({ content:  bot.getReplyContent(message, args.join(" "), true, true)?.data }).then(bot.shortMessage(message, `\`\`\`fix\n${args.join(" ")}\`\`\``, 'success', { name: `Message Edited By ${message?.author?.tag}`, icon_url: `${message?.author?.avatarURL()}`}, { text: `ID : ${msg?.id}` })).catch(err => console.log(err));
+    }
+} 
