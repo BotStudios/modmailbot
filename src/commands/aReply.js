4bab520760e7331147dfe69401f844d26967a522
CY L <91641514+w3cy@users.noreply.github.com>
2022-01-01 17:58:28 +0800

    Update aReply.js

--- src/commands/aReply.js
+++ src/commands/aReply.js
@@ -1,37 +1,15 @@
 module.exports = {
-    name: 'ar',
-    description: 'Reply to a thread/user anonymously',
+    name: 'ae',
+    description: 'Edit a replied message anonymously',
     options: [
-        { name: 'message', description: 'Message to reply' }
+        { name: '[message]', description: 'Reply to a message to be edited' }
     ],
-    run: async ({ bot, data, config, message, args }) => {
-        var content = message.content.slice(config?.prefix?.length+3); var userid; var repliedMsg; var replyMsgId;
-        if(args[0] && args[0].startsWith('--')) {
-          const tag = await bot.getTag(message.content.slice(config?.prefix?.length+5));
-          if(tag) { content = tag; bot.shortMessage(message, `${tag}`, 'success', { name: `${message.author.tag} (Anonymously)`, icon_url: `${message?.author?.avatarURL()}` }).then((m) => replyMsgId = m?.id);}
-        }
-        var replyContent = bot.getReplyContent(message, content, false, true);
-        if(!replyContent)return bot.shortMessage(message, 'Please provide a message', 'error');
-        if(message?.channel?.id == data.Channel) {
-            userid = data.User;
-        }else if(message?.channel?.id?.startsWith('ID:')){
-            userid = message?.channel?.id?.startsWith('ID:');
-        }
-        const user = await bot.client.users.cache.get(userid);
-        if(!user) return bot.shortMessage(message, 'This user does not exist.', 'error');
-        await user.send(replyContent.data).then(msg => {
-            repliedMsg = msg; 
-            if(!config?.customReply) {
-                message.react('✅').then(() => message.reply(`Successfully Send Message To <@!${data.User}> \`Anonymously\``).then(m => setTimeout(() => m.delete(), 2000)));
-            }else {
-                bot.emit('threadReply', message, replyContent.raw, data, true);
-            }
-        }).catch(err => { console.log(err); message.react('❌'); })
-        if (data){
-           data.Messages = bot.getContent(data?.Channel, message, data?.Messages || [], content, true).Messages;
-           bot.collection.set(data?.User, data);
-           if(replyMsgId && repliedMsg) bot.editMsg.set(`${replyMsgId}`, repliedMsg);
-           await data.save().catch((err) => { });
-        }
-    } 
-}
+    run: async ({ bot, data, message, args }) => {
+        if(!data)return;
+        if(!message?.reference?.messageId) return bot.shortMessage(message, 'Please refer/reply to a message.', 'error').then(m => setTimeout(() => m.delete(), 2000));
+        if(!args.join(" "))return bot.shortMessage(message, 'Please provide a message to be edited.', 'error').then(m => setTimeout(() => m.delete(), 2000));
+        var msg = bot.editMsg.get(message?.reference?.messageId);
+        if(!msg)return bot.shortMessage(message, 'This message cannot be edited.', 'error').then(m => setTimeout(() => m.delete(), 2000));
+        return await msg.edit({ content: bot.getReplyContent(message, args.join(" "), true, true)?.data }).then(bot.editedEmbed(message, args, msg, true)).catch(err => console.log(err));
+    }
+} 
