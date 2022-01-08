7a61dd075855a334b1dee93368758a309aa74d57
CY L <91641514+w3cy@users.noreply.github.com>
2022-01-08 17:35:16 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,23 +1,6 @@
 const { version } = require('./../package.json');
+const config = require('./../config.js');
 module.exports = {
     version,
-    databaseURI: '', // *
-    token: '', // *
-    guildID: '', // *
-    category: '', // *
-    roleID: '', // *
-    prefix: '',  // *
-    logThreads: true,
-    threadCloseDelay: 1500,
-    customReply: false,
-    notifyMsg: '',
-    webhookURI: '',
-    activity: '',
-    logsURI: '',
-    colors: { 
-        success: '', // *
-        error: '', // *
-        primary: '', // *
-        custom: '' // *
-    }
+    config
 }
