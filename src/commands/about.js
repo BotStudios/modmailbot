c48e8465a4ca57585f911385aeea119e91199294
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-22 16:36:06 +0800

    Dev: Update About Embed

--- src/commands/about.js
+++ src/commands/about.js
@@ -7,26 +7,26 @@ module.exports = {
         var hours = Math.floor(totalSeconds / 3600); totalSeconds %= 3600;
         var minutes = Math.floor(totalSeconds / 60),
         seconds = Math.floor(totalSeconds % 60);
-        return message.reply({ embeds: [new bot.Discord.MessageEmbed()
-           .setAuthor('Modmail - About',  bot?.client?.user?.avatarURL(), 'https://github.com/BotStudios/ModmailBot')
+        return message.reply({ embeds: [new bot.Discord.EmbedBuilder()
+           .setAuthor({ name: 'Modmail - About',  iconURL: bot?.client?.user?.avatarURL(), url: 'https://github.com/BotStudios/ModmailBot' })
            .setDescription('A feature rich [discord.js](https://github.com/discordjs/discord.js) Modmail Bot,\n highly inspired by other modmail bots out there.')
-           .addField('Author', '[`@joeleeofficial`](https://github.com/joeleeofficial), [`@leecheeyong`](https://github.com/leecheeyong), [`@w3cy`](https://github.com/w3cy)', false)
-           .addField('Uptime', `${days}d, ${hours}h, ${minutes}m and ${seconds}s`, true)
-           .addField('Latency', `${Date.now() - message.createdTimestamp}ms`, true)
-           .addField('API Latency', `${Math.round(bot?.client?.ws?.ping)}ms`, true)
-           .addField('Links', `
+           .addFields({ name: 'Author', value: '[`@joeleeofficial`](https://github.com/joeleeofficial), [`@leecheeyong`](https://github.com/leecheeyong), [`@w3cy`](https://github.com/w3cy)', inline: false })
+           .addFields({ name: 'Uptime', value: `${days}d, ${hours}h, ${minutes}m and ${seconds}s`, inline: true })
+           .addFields({ name: 'Latency', value: `${Date.now() - message.createdTimestamp}ms`, inline: true })
+           .addFields({ name: 'API Latency', value:`${Math.round(bot?.client?.ws?.ping)}ms`, inline: true })
+           .addFields({ name: 'Links', value: `
            [\`Github\`](https://github.com/BotStudios/ModmailBot)
            [\`Logs Viewer\`](https://github.com/BotStudios/logs-viewer)
            [\`Support Server\`](https://discord.com/invite/vYwH7Sa9N8)
            [\`Docs\`](https://github.com/BotStudios/ModmailBot/wiki)
-           `, true)
-           .addField('Information', `
+           `, inline: true })
+           .addFields({ name: 'Information', value: `
            *Version* : \`v${config?.version}\`
            *Log Threads* : \`${config?.logThreads ? 'Yes' : 'No'}\`
            *Prefix* : \`${config?.config?.prefix}\`
            *Staff Role* : <@&${config?.config?.roleID || 'Unset'}>
            *Logs URI* : \`${config?.config?.logsURI || 'Unset'}\`
-           `, true)
+           `, inline: true })
            .setTimestamp()
         ]});
         }
