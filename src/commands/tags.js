355f8b56b47825c3ed485d023fa2ebbf229390c5
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-22 16:37:32 +0800

    Dev: Update tags embed

--- src/commands/tags.js
+++ src/commands/tags.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'tags',
     description: 'View a list of created tags/snippets',
     run: async ({ bot, config, message, args }) => {
-        var embed = new bot.Discord.MessageEmbed().setAuthor('Snippets'); 
+        var embed = new bot.Discord.EmbedBuilder().setAuthor({ name: 'Snippets' }); 
         var tags = ""; var tagCount = 0;
         const data = await bot.getTags(); 
         for(var i in data) { tagCount++; tags += `${tagCount}. ${i}\n`; }
         if(tagCount == 0) tags += `The tag list was empty at the moment`;
-        embed.setDescription(tags).setColor('RANDOM'); message.reply({ embeds: [embed] });
+        embed.setDescription(tags); message.reply({ embeds: [embed] });
     }
 }
