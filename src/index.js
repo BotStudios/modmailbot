eb627df9be39ad8825a7b0e5487050a61a8efffb
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-22 16:33:56 +0800

    Dev: Client Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 'use strict';
 const Discord = require('discord.js');
+const { GatewayIntentBits, Partials, ChannelType } = require('discord.js');
 const collection = new Discord.Collection();
 const client = new Discord.Client({ 
-    intents: [Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_PRESENCES], 
-    partials: ["MESSAGE", "CHANNEL", "REACTION"] 
+    intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences, GatewayIntentBits.MessageContent], 
+    partials: [ Partials.Message, Partials.Channel, Partials.Reaction] 
 });     
 const utils = new (require('./manager'))(client, collection);
-
+ 
 client.once('ready', async () => {
   await utils.ready(); 
   await utils.error.check() 
@@ -15,17 +16,16 @@ client.once('ready', async () => {
 client.on('messageCreate',  async (message) => {
   if(message.author.bot)return; 
   try{ 
-    if(message.channel.type == 'DM') {        
+    if(message.channel.type == ChannelType.DM) {        
     if(!collection.get(message.author.id))return await utils.createChannel(message);
      await utils.sendInbox(message);
   }
-  if(message.content.startsWith(utils?.config?.prefix) && message.channel.type == 'GUILD_TEXT') return await utils.command(message);
-  if(message.channel.type == 'GUILD_TEXT')return await utils.replyThread(message);
+  if(message.content.startsWith(utils?.config?.prefix) && message.channel.type == ChannelType.GuildText) return await utils.command(message);
+  if(message.channel.type == ChannelType.GuildText)return await utils.replyThread(message);
 }catch(e) {
     console.log(e)
     message.react("❌")
 }
 })
-
 
 client.login(utils.config.token);
